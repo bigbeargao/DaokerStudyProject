8d8e59044dc22664684104f20334352c53095109
gaoweifeng <944694659@qq,com>
2019-09-10 15:52:42 +0800

    123

--- demo/src/main/java/com/example/demo/GaoweifengTestController.java
+++ demo/src/main/java/com/example/demo/GaoweifengTestController.java
@@ -1,17 +1,42 @@
 package com.example.demo;
 
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.NumberUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import java.math.BigDecimal;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class GaoweifengTestController {
-    @GetMapping(value = "/rwere")
-    public String gdsag() {
-        System.out.println("23423423");
-        return "124342";
+    public void b() {
+        AtomicInteger atomicInteger = new AtomicInteger(5);
+        atomicInteger.getAndSet(5);
+        Integer.toString(1);
+    }
+
+
+    @GetMapping(value = "/x")
+    public void a() {
+        //新建一个线程
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }).start();
+
+        //新建一个线程池
+        Executor executor = Executors.newCachedThreadPool();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
     }
 }
